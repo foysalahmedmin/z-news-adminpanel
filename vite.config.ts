82ca959a6788f2a4ab2951b4da0035f321d6ee04
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-08-23 14:09:16 +0600

    ---Update

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
+    },
+  },
+  define: {
+    global: "globalThis",
+  },
+  base: "./",
 });
