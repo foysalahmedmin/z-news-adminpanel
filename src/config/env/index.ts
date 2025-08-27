db5caf54fe516d858e5eceba42cd56706115b63d
foysalahmedmin <foysalahmedmin@gmail.com>
2025-08-28 03:41:45 +0600

    Update

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,10 +1,10 @@
 export const ENV = {
-  api_url: (import.meta.env.VITE_API_URL as string) || "http://localhost:3000",
-  app_url: (import.meta.env.VITE_APP_URL as string) || "http://localhost:8080",
-  // api_url:
-  //   (import.meta.env.VITE_API_URL as string) ||
-  //   "https://z-news-server.vercel.app",
-  // app_url:
-  //   (import.meta.env.VITE_APP_URL as string) || "https://z-news.vercel.app",
+  // api_url: (import.meta.env.VITE_API_URL as string) || "http://localhost:3000",
+  // app_url: (import.meta.env.VITE_APP_URL as string) || "http://localhost:8080",
+  api_url:
+    (import.meta.env.VITE_API_URL as string) ||
+    "https://z-news-server.vercel.app",
+  app_url:
+    (import.meta.env.VITE_APP_URL as string) || "https://z-news.vercel.app",
   environment: import.meta.env.MODE as "development" | "production",
 };
