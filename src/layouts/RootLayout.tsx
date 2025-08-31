be7b7bb048bcdfa76767930a116b0b2792af5b58
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-08-31 18:15:46 +0600

    -

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,12 +2,14 @@ import MenuApplier from "@/components/appliers/MenuApplier";
 import SettingApplier from "@/components/appliers/SettingApplier";
 import { Outlet } from "react-router";
 import { Slide, ToastContainer } from "react-toastify";
+import NotificationApplier from "../components/appliers/NotificationApplier";
 
 const RootLayout = () => {
   return (
     <>
       <MenuApplier />
       <SettingApplier />
+      <NotificationApplier />
       <ToastContainer
         position="top-center"
         autoClose={5000}
