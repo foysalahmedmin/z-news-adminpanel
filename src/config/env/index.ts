057f1f19cab5982eac8aa604f8236b8ad09282c5
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-09-18 14:52:47 +0600

    Update App Url

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,6 @@
 export const ENV = {
   api_url:
     (import.meta.env.VITE_API_URL as string) || "https://admin.dainikeidin.com",
-  app_url:
-    (import.meta.env.VITE_APP_URL as string) || "http://test.dainikeidin.com",
+  app_url: (import.meta.env.VITE_APP_URL as string) || "http://dainikeidin.com",
   environment: import.meta.env.MODE as "development" | "production",
 };
