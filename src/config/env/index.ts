d07e2b3c1fb8b8c30522c3b3dbe543f217a3e71c
Foysal Ahmed <foysalahmedmin@gmail.com>
2026-02-03 21:22:57 +0600

    Update ENV config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,9 +1,9 @@
 export const ENV = {
-  api_url:
-    (import.meta.env.VITE_API_URL as string) ||
-    "https://z-news-server.vercel.app",
   app_url:
     (import.meta.env.VITE_APP_URL as string) ||
+    "https://z-news-server.vercel.app",
+  api_url:
+    (import.meta.env.VITE_API_URL as string) ||
     "https://z-news-server.vercel.app",
   environment: import.meta.env.MODE as "development" | "production",
   google_client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
