a22fd5b321ca407632c068e76937549619d0c28b
foysalahmedmin <foysalahmedmin@gmail.com>
2026-02-03 17:02:31 +0600

    Update ENV config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,10 @@
 export const ENV = {
   api_url:
-    (import.meta.env.VITE_API_URL as string) || "https://admin.z-news.com",
-  app_url: (import.meta.env.VITE_APP_URL as string) || "http://z-news.com",
+    (import.meta.env.VITE_API_URL as string) ||
+    "https://z-news-server.vercel.app",
+  app_url:
+    (import.meta.env.VITE_APP_URL as string) ||
+    "https://z-news-server.vercel.app",
   environment: import.meta.env.MODE as "development" | "production",
   google_client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
 };
